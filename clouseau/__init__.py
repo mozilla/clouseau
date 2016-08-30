5bf5cb2b00bbc883362e6041689b60131f67b801
Calixte Denizet <calixte.denizet@gmail.com>
2016-08-30 19:42:58 +0200

    __init__: replace '..' by os.path.dirname

--- clouseau/__init__.py
+++ clouseau/__init__.py
@@ -3,6 +3,6 @@ import libmozdata.config as config
 
 
 here = os.path.abspath(__file__)
-path = os.path.join(here, '../config.ini')
+path = os.path.join(os.path.dirname(here), 'config.ini')
 path = os.path.abspath(path)
 config.set_config(config.ConfigIni(path))
