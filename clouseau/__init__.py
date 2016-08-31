c10e89c3c22356dff0277255fb15f2b78218bbfe
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-08-31 19:27:38 +0200

    Update to libmozdata 0.1.1 and remove no longer needed hack for the config file

--- clouseau/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import libmozdata.config as config
-
-
-here = os.path.abspath(__file__)
-path = os.path.join(os.path.dirname(here), 'config.ini')
-path = os.path.abspath(path)
-config.set_config(config.ConfigIni(path))
