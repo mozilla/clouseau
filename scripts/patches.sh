be542351e12ee231f7d991dbc83383c60791f334
Calixte Denizet <calixte.denizet@gmail.com>
2016-11-22 14:46:15 +0100

    Add -l option to analyze bts in using local hg repo

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -6,6 +6,6 @@ hg --config extensions.hgmo=$1/hg/version-control-tools.hg/hgext/hgmo serve --hg
 pid=$!
 
 cd $1/git/clouseau
-python -m clouseau.guiltypatches -d $3 -o $4 -c $5 -t $6 
+python -m clouseau.guiltypatches -d $3 -o $4 -c $5 -t $6 -l
 
 kill $pid
