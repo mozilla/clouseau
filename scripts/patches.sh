34784b47963b7d3edbc66cac654ad18419d9e834
Calixte Denizet <calixte.denizet@gmail.com>
2016-11-22 14:44:06 +0100

    Fix path to hgmo in patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -2,7 +2,7 @@
 
 cd $1/hg/mozilla-$2.hg
 hg pull -u
-hg --config extensions.hgmo=$1/version-control-tools.hg/hgext/hgmo serve --hgmo &
+hg --config extensions.hgmo=$1/hg/version-control-tools.hg/hgext/hgmo serve --hgmo &
 pid=$!
 
 cd $1/git/clouseau
